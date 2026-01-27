37c0c5f1debc5334449e241119920bb7c867adae
Jaroslav Kirichok <kirichok.jaroslav@gmail.com>
2026-01-27 16:27:07 +0200

    chore: add a small reinsurance

--- src/api/models.rs
+++ src/api/models.rs
@@ -14,22 +14,14 @@ pub struct LetsStartForm {
         exclusive_max = 50_000,
         message = "The budget must range from 1,000 to 50,000 USD"
     ))]
-    pub min_budget: u16,
-    #[validate(range(
-        min = 1_000,
-        max = 50_000,
-        message = "The budget must range from 1,000 to 50,000 USD"
-    ))]
-    pub max_budget: u16,
+    pub min_budget: u32,
+    #[validate(range(min = 1_000, max = 50_000, message = "The budget must range from 1,000 to 50,000 USD"))]
+    pub max_budget: u32,
 
     #[validate(length(min = 2, max = 32, message = "The name must be between 2 and 32 chars"))]
     pub name: String,
 
-    #[validate(length(
-        min = 64,
-        max = 512,
-        message = "The project description must be between 64 and 512 chars"
-    ))]
+    #[validate(length(min = 64, max = 512, message = "The project description must be between 64 and 512 chars"))]
     pub project_description: String,
 }
 
