fd85d39d49869ddbad63eba6cac5c54550f96207
Jaroslav Kirichok <kirichok.jaroslav@gmail.com>
2026-01-30 21:24:53 +0200

    chore: add tiny changes

--- src/api/handlers.rs
+++ src/api/handlers.rs
@@ -18,7 +18,7 @@ pub async fn alive_handler() -> Json<StaticResponse> {
 pub async fn send_message_handler(
     State(state): State<Arc<AppState>>, ApiJsonRequest(form): ApiJsonRequest<LetsStartForm>,
 ) -> Result<Json<StaticResponse>, ApiErrorResponse> {
-    state.mailer.send_message(form, &state.configs).await?;
+    state.mailer.send_message(form, &state.app_configs).await?;
 
     Ok(Json(StaticResponse::message("The message was successfully sent")))
 }
